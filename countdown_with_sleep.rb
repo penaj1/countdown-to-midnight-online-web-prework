6bd42fccc0699c7dbae71644277ce16da6290481
Janice Pena <jipena94@gmail.com>
2019-04-03 04:10:22 +0000

    Automatically backed up by Learn

--- countdown_with_sleep.rb
+++ countdown_with_sleep.rb
@@ -1,12 +1,1 @@
 #write your code here
-
-def countdown_with_sleep(number)
-while number > 0
-  sleep 1
-  puts "#{number} SECOND(S)!"
-  number -= 1
-end
-  return "HAPPY NEW YEAR!"
-end
-
-countdown(5)
